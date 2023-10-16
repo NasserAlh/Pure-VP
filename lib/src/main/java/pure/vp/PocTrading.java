189e572e17fa7e9b8ebf300e0a9ebfe761aeb9b2
Nasser H <nassq8hh@gmail.com>
2023-10-16 20:54:54 +0300

    Standard Deviation

--- lib/src/main/java/pure/vp/PocTrading.java
+++ lib/src/main/java/pure/vp/PocTrading.java
@@ -5,8 +5,8 @@ import java.awt.image.BufferedImage;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Map;
-import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.LinkedList;
+import java.util.TreeMap;
 import java.util.stream.IntStream;
 
 import javax.swing.JSlider;
@@ -18,32 +18,39 @@ import velox.api.layer1.simplified.*;
 import velox.gui.StrategyPanel;
 
 @Layer1SimpleAttachable
-@Layer1StrategyName("Enhanced POC with Volume Logic")
+@Layer1StrategyName("Optimized POC")
 @Layer1ApiVersion(Layer1ApiVersionValue.VERSION1)
 public class PocTrading implements CustomModule, TradeDataListener, TimeListener, CustomSettingsPanelProvider {
 
-    private Indicator priceIndicator, volumeIndicator, pocIndicator, PocTrade;
-    private ConcurrentSkipListMap<Double, Integer> volumeProfile = new ConcurrentSkipListMap<>();
-    private double previousPrice = Double.NaN, pointOfControl = Double.NaN;
+    private Indicator priceIndicator, pocIndicator, pocTradeIndicator;
+    private TreeMap<Double, Integer> volumeProfile = new TreeMap<>();  // Updated Line
+    private double previousPrice = Double.NaN;
     private long startTime = 0, currentTimestamp;
     private final long TIME_LIMIT = 120 * 1_000_000_000L;
     private volatile double POC_BUFFER = 2;
-    
-    // New Class Variables
+    private static final ZoneId NY_ZONE_ID = ZoneId.of("America/New_York");
+    private double pointOfControlPrice = Double.NaN;
+    private int pointOfControlVolume = 0;
+    private static final int START_HOUR = 9;
+    private static final int START_MINUTE = 45;
+    private static final double VOLUME_TRIGGER_THRESHOLD = 0.00;
+    private static final int POC_ROBUSTNESS_THRESHOLD = 0;
     private int volumeSum = 0;
     private int prevVolumeSum = 0;
+    private LinkedList<Double> recentPrices = new LinkedList<>();
+    private static final int PRICE_WINDOW_SIZE = 50;  // Define your window size
+
 
     @Override
     public void initialize(String alias, InstrumentInfo info, Api api, InitialState initialState) {
         Indicator[] indicators = {
             priceIndicator = api.registerIndicator("Price Indicator", GraphType.PRIMARY, Double.NaN),
-            volumeIndicator = api.registerIndicator("Volume Indicator", GraphType.BOTTOM, Double.NaN),
             pocIndicator = api.registerIndicator("POC Indicator", GraphType.PRIMARY, Double.NaN)
         };
 
-        PocTrade = api.registerIndicator("Trade Signal", GraphType.PRIMARY);
+        pocTradeIndicator = api.registerIndicator("Trade Signal", GraphType.PRIMARY);
         
-        Color[] colors = {Color.BLUE, Color.YELLOW, Color.RED};
+        Color[] colors = {Color.BLUE, Color.RED};
 
         IntStream.range(0, indicators.length)
                 .forEach(i -> indicators[i].setColor(colors[i]));
@@ -52,42 +59,61 @@ public class PocTrading implements CustomModule, TradeDataListener, TimeListener
     @Override
     public synchronized void onTrade(double price, int size, TradeInfo tradeInfo) {
 
-        // Update Cumulative Volume
         volumeSum += size;
+        double volumeRateOfChange = calculateVolumeRateOfChange();
 
-        // Rate of Change in Volume
-        double volumeRateOfChange = prevVolumeSum == 0 ? 0 : (double) (volumeSum - prevVolumeSum) / prevVolumeSum;
-
-        ZonedDateTime currentTime = ZonedDateTime.ofInstant(Instant.ofEpochSecond(currentTimestamp / 1_000_000_000), ZoneId.of("America/New_York"));
-        ZonedDateTime startTimeNY = ZonedDateTime.ofInstant(Instant.ofEpochSecond(startTime / 1_000_000_000), ZoneId.of("America/New_York")).withHour(9).withMinute(45);
+        ZonedDateTime currentTime = ZonedDateTime.ofInstant(Instant.ofEpochSecond(currentTimestamp / 1_000_000_000), NY_ZONE_ID);
+        ZonedDateTime startTimeNY = currentTime.withHour(START_HOUR).withMinute(START_MINUTE).withDayOfYear(currentTime.getDayOfYear()).withYear(currentTime.getYear());
 
         if (currentTime.isBefore(startTimeNY)) {
             return;
         }
 
         priceIndicator.addPoint(price);
-        volumeProfile.merge(price, size, (a, b) -> a != null && b != null ? Integer.sum(a, b) : a != null ? a : 0);
-        volumeIndicator.addPoint(volumeProfile.get(price));
 
-        pointOfControl = volumeProfile.entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse(price);
-        pocIndicator.addPoint(pointOfControl);
+        // New code to update recent price data
+        recentPrices.add(price);
+        if (recentPrices.size() > PRICE_WINDOW_SIZE) {
+            recentPrices.removeFirst();
+        }
+        // New code to calculate standard deviation
+        double standardDeviation = calculateStandardDeviation();
 
-        int pocRobustness = volumeProfile.getOrDefault(pointOfControl, 0);
+        // Assume a higher standard deviation indicates more volatility
+        // and adjust your trading logic accordingly, e.g.,
+        boolean isHighVolatility = standardDeviation > 0.5;  // 0.5 is the threshold for high volatility
+
+        volumeProfile.merge(price, size, Integer::sum);
+        int currentPriceVolume = volumeProfile.get(price);
+        if (currentPriceVolume > pointOfControlVolume) {
+            pointOfControlPrice = price;
+            pointOfControlVolume = currentPriceVolume;
+        }
+        pocIndicator.addPoint(pointOfControlPrice);  // Updated Line
+
+        int pocRobustness = volumeProfile.getOrDefault(pointOfControlPrice, 0);  // Updated Line
 
         if (currentTimestamp - startTime >= TIME_LIMIT && !Double.isNaN(previousPrice)) {
-            boolean enteredUpper = previousPrice > pointOfControl + POC_BUFFER && price <= pointOfControl + POC_BUFFER;
-            boolean exitedLower = previousPrice < pointOfControl - POC_BUFFER && price >= pointOfControl - POC_BUFFER;
+            boolean enteredUpper = previousPrice > pointOfControlPrice + POC_BUFFER && price <= pointOfControlPrice + POC_BUFFER;  // Updated Line
+            boolean exitedLower = previousPrice < pointOfControlPrice - POC_BUFFER && price >= pointOfControlPrice - POC_BUFFER;  // Updated Line
 
-            boolean volumeTrigger = volumeRateOfChange > 0.00; // A 5% increase in volume
-            boolean pocStable = pocRobustness > 0; // POC has been the same for more than 5 trades
+            boolean volumeTrigger = volumeRateOfChange > VOLUME_TRIGGER_THRESHOLD; // A x% increase in volume
+            boolean pocStable = pocRobustness > POC_ROBUSTNESS_THRESHOLD; // POC has been the same for more than x trades
 
-            if ((enteredUpper || exitedLower) && volumeTrigger && pocStable) {
-                PocTrade.addIcon(price, createTranslucentCircle(enteredUpper), 1, 1);
+            // Updated trading logic to include volatility check
+        if ((enteredUpper || exitedLower) && volumeTrigger && pocStable && isHighVolatility) {
+            pocTradeIndicator.addIcon(price, createTranslucentCircle(enteredUpper), 1, 1);
             }
         }
 
         previousPrice = price;
         prevVolumeSum = volumeSum;
+    }
+
+    private double calculateStandardDeviation() {
+        double mean = recentPrices.stream().mapToDouble(val -> val).average().orElse(0.0);
+        double variance = recentPrices.stream().mapToDouble(val -> Math.pow(val - mean, 2)).sum() / recentPrices.size();
+        return Math.sqrt(variance);
     }
 
     private BufferedImage createTranslucentCircle(boolean isBid) {
@@ -98,6 +124,10 @@ public class PocTrading implements CustomModule, TradeDataListener, TimeListener
         g.fillOval(0, 0, diameter, diameter);
         g.dispose();
         return icon;
+    }
+
+    private double calculateVolumeRateOfChange() {
+        return prevVolumeSum == 0 ? 0 : (double) (volumeSum - prevVolumeSum) / prevVolumeSum;
     }
 
     @Override
